82b4e91f9b588f620374d6410af51aad372c6715
Lindsay Chapin <lnchapin@gmail.com>
2018-02-13 13:36:04 -0700

    init

---
+++ dentallab/src/components/Header.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class Header extends React.Component {
+  render(){
+    return(
+    <div className='header-container'>
+      <h1>Dental Lab Quiz</h1>
+    </div>
+  )
+  }
+}
+
+export default Header;
