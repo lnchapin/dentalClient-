625d4aeb9c93f3d59d0aaa8161b563c638ec4237
Lindsay Chapin <lnchapin@gmail.com>
2018-04-22 10:38:31 -0600

    wip

--- dentallab/src/App.js
+++ dentallab/src/App.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import Quiz from './components/Quiz'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <header className="App-header">
+        <Quiz />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
---
+++ dentallab/src/components/Quiz.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+return a;
+}
+
+class QuestionClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      questions: [],
+      correctAnswer: []
+  }
+}
+
+  async getQuestions() {
+    try{
+      let response = await fetch(`http://localhost:3030/api/v1/questions`)
+      .then(response => response.json())
+      console.log(response)
+      for (var i = 0; i < response.length; i++) {
+        var answerChoices = []
+        answerChoices.push(response[i].correctAnswer, response[i].incorrectAnswer1, response[i].incorrectAnswer2, response[i].incorrectAnswer3);
+        this.state.questions.push([response[i].questionText, shuffle(answerChoices)])
+        this.state.correctAnswer.push(response[i].correctAnswer)
+      }
+      this.setState({
+        isLoading: false
+      })
+      console.log('post loop questions', this.state.questions)
+      console.log('correc arr', this.state.correctAnswer);
+    } catch(error) {
+      console.error(error);
+    }
+}
+
+componentDidMount(){
+  this.getQuestions();
+}
+
+  render() {
+    if(this.state.isLoading === true) {
+      return (
+        <div>Loading...</div>
+      )
+    } else if(this.state.isLoading === false) {
+    return(
+          <div>
+            <form>
+            {this.state.questions.map(answer =>
+             <div>
+               <h3>{answer[0]}</h3>
+               <label>
+
+               {answer[1].map(option => {<input type="radio" value="option1" />, {option}})}
+               </label>
+             </div>)}
+             </form>
+          </div>
+      )
+    }
+  }
+}
+export default QuestionClass;
+
+{/* <div>
+  {this.state.questions.map(answer =>
+   <div>
+     <h3>{answer[0]}</h3>
+     {answer[1].map(options => <li>{options}</li>)}
+   </div>)}
+</div> */}
